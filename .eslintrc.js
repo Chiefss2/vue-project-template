e248f1eae6325a3ff4948c3a99cf089d02cf7b23
Chiefss2 <chiefs2@163.com>
2019-05-23 19:36:12 +0800

    install ant-design-vue and moment

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     node: true
   },
   // extends: ["plugin:vue/essential", "@vue/prettier"],
-  extends: ['plugin:vue/strongly-recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -1,9 +1,9 @@
 import { mount } from "@vue/test-utils";
-import sinon from 'sinon';
+import sinon from "sinon";
 import Counter from "@/components/Counter.vue";
 
 describe("Counter.vue", () => {
-  let isCalled = false;
+  // let isCalled = false;
   const change = sinon.spy();
   const wrapper = mount(Counter, {
     listeners: {
@@ -17,13 +17,13 @@ describe("Counter.vue", () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
   it("count++ and change isCalled", () => {
-    const button = wrapper.find('button');
-    button.trigger('click');
+    const button = wrapper.find("button");
+    button.trigger("click");
     expect(wrapper.vm.count).toBe(1);
     // expect(isCalled).toBe(true);
     // expect(change.called).toBe(true);
     expect(change.callCount).toBe(1);
-    button.trigger('click');
+    button.trigger("click");
     expect(change.callCount).toBe(2);
   });
 });
