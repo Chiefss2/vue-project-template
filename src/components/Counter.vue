67d2873007d51858f5d3203b7126436cbfbdeed0
Chiefss2 <chiefs2@163.com>
2019-05-23 19:10:27 +0800

    install less-loader

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="counter">
+    <span>count: {{ count }}</span>
+    <button @click="handleClick">count++</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    handleClick() {
+      this.count++;
+      this.$emit('change', this.count);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
