54a3645f114ff084f876e3b4758761d01be3e396
Chiefss2 <chiefs2@163.com>
2019-05-26 14:33:32 +0800

    精细化权限设计（权限组件、权限指令）

---
+++ src/directives/auth.js
@@ -0,0 +1,18 @@
+import { check } from "../utils/auth";
+
+// 权限指令弊端：只有第一次渲染时才会控制，无法解决动态权限改变的问题
+// 权限组件唯一缺点：就是写法复杂点 e.g.
+// <Authorized :authority="['admin']">
+//   <SettingDrawer />
+// </Authorized>
+function install(Vue, options = {}) {
+  Vue.directive(options.name || "auth", {
+    inserted(el, binding) {
+      if (!check(binding.value)) {
+        el.parentNode && el.parentNode.removeChild(el);
+      }
+    }
+  });
+}
+
+export default { install };
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import store from "./store/store";
 import "./registerServiceWorker";
 import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
+// 使用权限组件控制权限
+import Authorized from "./components/Authorized";
+// 使用自定义权限指令控制权限
+import Auth from "./directives/auth";
 
 Vue.config.productionTip = false;
 Vue.use(Button);
@@ -12,6 +16,8 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+Vue.component("Authorized", Authorized); // 注册全局组件
+Vue.use(Auth); // 注册全局指令
 
 new Vue({
   router,
