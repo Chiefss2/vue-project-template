91b7ab22ec134778b5c2af1953e6a752866f37ba
Chiefss2 <chiefs2@163.com>
2019-05-26 16:32:55 +0800

    Using third-party libraries in components

---
+++ src/components/Chart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div ref="chartDom"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import debounce from "lodash/debounce";
+import { addListener, removeListener } from "resize-detector";
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    option(newOpt) {
+      this.chart.setOption(newOpt);
+    }
+    // 深度监听很耗费性能
+    // option: {
+    //   handler(newOpt) {
+    //     this.chart.setOption(newOpt);
+    //   },
+    //   deep: true
+    // }
+  },
+  created() {
+    this.resize = debounce(this.resize, 300);
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.chartDom, this.resize);
+  },
+  beforeDestory() {
+    removeListener(this.refs.chartDom, this.resize);
+    this.chart.dispose();
+    this.chart = null;
+  },
+  methods: {
+    resize() {
+      // window.console.log("resize");
+      this.chart.resize();
+    },
+    renderChart() {
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption(this.option);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
